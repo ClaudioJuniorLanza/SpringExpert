cfe4c9fee24d4f6faf99a387888a815589eeeb30
conectasementes <sistemas@conectasementes.com.br>
2020-12-04 18:24:09 -0300

    gerando o token e decodificando o token

---
+++ src/main/java/io/github/claudiojuniorlanca/JwtService.java
@@ -0,0 +1,70 @@
+package io.github.claudiojuniorlanca;
+
+import io.github.claudiojuniorlanca.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.expiracao}")
+    private String expiracao;
+
+    @Value("${security.jwt.chave-assinatura}")
+    private String chaveAssinatura;
+
+    private String gerarToken(Usuario usuario){
+        long expString = Long.valueOf(expiracao);
+        LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
+        Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+        Date data = Date.from(instant);
+
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put("emaildousuario", "email@email.com");
+        claims.put("roles", "admin");
+
+        return Jwts
+                .builder()
+                .setSubject(usuario.getLogin())
+                .setExpiration(data)
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.ES512, chaveAssinatura)
+                .compact();
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token){
+        try{
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            return !LocalDateTime.now().isAfter(data);
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+}
